db09c0b2393f678d116e64d8487ab102e27cbe1b
Rodave Joseph Bobadilla <rbobadilla@isbx.com>
2019-05-22 22:34:03 +0800

    Add left background

--- src/screens/SignupScreen.tsx
+++ src/screens/SignupScreen.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const LeftDiv = styled.img`
+  background-color: red;
+  width: 50vw;
+  height: 100vh;
+`;
+
+const RightDiv = styled.div`
+  background-color: pink;
+  width: 50vw;
+  height: 100vh;
+`;
 
 const SignupScreen: React.FC = () => {
-  return <div>signup</div>;
+  return (
+    <Container>
+      <LeftDiv src={require("../assets/images/launchBG.png")} />
+      <RightDiv />
+    </Container>
+  );
 };
 
 export default SignupScreen;
