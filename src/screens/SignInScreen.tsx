8ce95f64baad1cee46c88407a3bd00b721f4e0cf
aldrin027 <aldrin.gauran@gmail.com>
2019-05-24 14:46:11 +0800

    Modified Login Page

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,104 +1,117 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import FormControl from "@material-ui/core/FormControl";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import Paper from "@material-ui/core/Paper";
-import withStyles from "@material-ui/core/styles/withStyles";
-import { Theme, createStyles, Grid, Link, Typography } from "@material-ui/core";
+import { RouteComponentProps, withRouter } from "react-router";
 import LaunchContainer from "../containers/LaunchContainer";
 import LaunchTitle from "../components/LaunchTitle";
 
+import { Grid, TextField, FormControlLabel, Checkbox, Button, Typography, withStyles, createStyles, Theme, Link } from "@material-ui/core";
+
 const styles = (theme: Theme) =>
   createStyles({
-    main: {
-      width: "auto",
-      display: "block", // Fix IE 11 issue.
-      marginLeft: theme.spacing.unit * 3,
-      marginRight: theme.spacing.unit * 3,
-      [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-        width: 400,
-        marginLeft: "auto",
-        marginRight: "auto"
-      }
-    },
     paper: {
-      marginTop: theme.spacing.unit * 8,
+      margin: `${theme.spacing.unit * 8}px 20%`,
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
       padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
         .spacing.unit * 3}px`
     },
-    avatar: {
-      margin: theme.spacing.unit,
-      backgroundColor: theme.palette.secondary.main
-    },
     form: {
       width: "100%", // Fix IE 11 issue.
-      marginTop: theme.spacing.unit
+      marginTop: theme.spacing.unit + 30
     },
-    submit: {
-      marginTop: theme.spacing.unit * 3
+    button: {
+      width: 185
+    },
+    forgot_password: {
+      color: ''
     }
   });
 
-function SignInScreen(props: { classes: any }) {
-  const { classes } = props;
-
+const Signin: React.FC<{ classes: any } & RouteComponentProps> = ({
+  classes,
+  history
+}) => {
   return (
-    <main className={classes.main}>
-      <CssBaseline />
-      <Paper className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          R E G A S C O
-        </Typography>
+    <LaunchContainer>
+      <div className={classes.paper}>
+        <LaunchTitle subtitle="Welcome back! Please login to your account." />
         <form className={classes.form}>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="email">Email Address</InputLabel>
-            <Input id="email" name="email" autoComplete="email" autoFocus />
-          </FormControl>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="password">Password</InputLabel>
-            <Input
-              name="password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-          </FormControl>
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Link>
-            Forgot Password
-          </Link>
-          <Grid container direction="row" alignItems="flex-end" justify="space-between">
+          <Grid container spacing={24}>
+            <Grid item xs={12} sm={12}>
+              <TextField
+                required
+                id="username"
+                name="username"
+                label="Username"
+                fullWidth
+                autoComplete="username"
+              />
+            </Grid>
+            <Grid item xs={12} sm={12}>
+              <TextField
+                required
+                id="password"
+                name="password"
+                label="Password"
+                type="password"
+                fullWidth
+                autoComplete="password"
+              />
+            </Grid>
+            
+          </Grid>
+          
+          <Grid container item xs={12} direction="row" justify="space-between" alignItems="center">
+            <Grid item>
+              <FormControlLabel
+                  control={
+                    <Checkbox color="primary" name="saveAddress" value="yes"/>
+                  }
+                  label="Remember Me"
+                />
+            </Grid>
+            <Grid item >
+              <Typography
+                style={{cursor: "pointer"}}
+              >
+                Forgot Password
+              </Typography>
+            </Grid>
+          </Grid>
+          
+          <Grid container direction="row" justify="space-between" alignItems="baseline">
+            <Grid item>
               <Button
                 type="submit"
+                // fullWidth
                 variant="contained"
                 color="primary"
-                className={classes.submit}
+                className={classes.button}
               >
                 Login
               </Button>
+            </Grid>
+            <Grid item>
               <Button
                 type="submit"
+                // fullWidth
                 variant="outlined"
                 color="default"
-                className={classes.submit}
+                style={{color: '#3F51B5', backgroundColor: '#FFFFFF', outlineColor: '#3F51B5'}}
+                className={classes.button}
               >
                 Sign up
               </Button>
+            </Grid>
+          </Grid>
+          
+          <Grid>
+            
           </Grid>
         </form>
-      </Paper>
-    </main>
+      </div>
+    </LaunchContainer>
   );
-}
+};
 
-export default withStyles(styles)(SignInScreen);
+export default withRouter(withStyles(styles)(Signin));
