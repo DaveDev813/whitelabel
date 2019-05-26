382b14b182eb7d6ca9d885a24ffa5ed761d5ef51
Rodave Joseph Bobadilla <rbobadilla@isbx.com>
2019-05-26 15:14:11 +0800

    check subtitle

--- src/components/LaunchTitle.tsx
+++ src/components/LaunchTitle.tsx
@@ -4,21 +4,22 @@ import Typography from "@material-ui/core/Typography";
 interface LaunchTitleProps {
   subtitle?: string;
 }
-
 const LaunchTitle: React.FC<LaunchTitleProps> = ({ subtitle }) => {
   return (
     <React.Fragment>
       <Typography component="h1" variant="h5">
         REGASCO
       </Typography>
-      <Typography
-        style={{ marginTop: "12px" }}
-        align="center"
-        color="textSecondary"
-        component="p"
-      >
-        {subtitle}
-      </Typography>
+      {subtitle && (
+        <Typography
+          style={{ marginTop: "12px" }}
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          {subtitle}
+        </Typography>
+      )}
     </React.Fragment>
   );
 };
